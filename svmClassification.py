72e52c21600d21e38f575db01cb3d6617ba8df85
Simon de Wit <simondewit@MacBook-Pro-van-Simon-de-Wit.local>
2017-10-16 15:47:19 +0200

    ja

--- svmClassification.py
+++ svmClassification.py
@@ -1,6 +1,7 @@
 import pickle
 import sys
 import numpy as np
+import string
 
 import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,10 +12,7 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 from nltk.tokenize import TweetTokenizer
 from sklearn import svm
-
-#==================================================================
-
-import string
+from sklearn.base import BaseEstimator, TransformerMixin
 
 from nltk.corpus import stopwords as sw
 from nltk.corpus import wordnet as wn
@@ -22,39 +20,6 @@ from nltk import wordpunct_tokenize
 from nltk import WordNetLemmatizer
 from nltk import sent_tokenize
 from nltk import pos_tag
-
-from sklearn.base import BaseEstimator, TransformerMixin
-
-class CustomPreprocessor(BaseEstimator, TransformerMixin):
-
-    def __init__(self, stopwords=None, punct=None,
-                 lower=True, strip=True):
-        self.lower      = lower
-        self.strip      = strip
-        #self.stopwords  = stopwords or set(sw.words('english'))
-
-    def fit(self, X, y=None):
-        return self
-
-    def inverse_transform(self, X):
-        return [" ".join(doc) for doc in X]
-
-    def transform(self, X):
-        test = [list(self.tokenize(doc)) for doc in X]
-        print(test)
-        return test
-
-    def tokenize(self, document):
-        tokenizer = TweetTokenizer()
-        tokenized_tweet = tokenizer.tokenize(document)
-        # Tokenize tweet
-        for token in tokenized_tweet:
-            # Apply preprocessing to the token
-            token = token.lower() if self.lower else token
-            token = token.strip() if self.strip else token
-            yield token
-
-#==================================================================
 
 
 def main():
@@ -77,13 +42,47 @@ def main():
 	evaluation = evaluate(results, test_tweets, test_categories)
 	print(evaluation)
 	
+class CustomPreprocessor(BaseEstimator, TransformerMixin):
+
+    def __init__(self, stopwords=None, punct=None,
+                 lower=True, strip=True):
+        self.lower      = lower
+        self.strip      = strip
+        #self.stopwords  = stopwords or set(sw.words('english'))
+
+    def fit(self, X, y=None):
+        return self
+
+    def inverse_transform(self, X):
+        return [" ".join(doc) for doc in X]
+
+    def transform(self, X):
+        test = [list(self.tokenize(doc)) for doc in X]
+        return test
+
+    def tokenize(self, document):
+        tokenizer = TweetTokenizer()
+        tokenized_tweet = tokenizer.tokenize(document)
+        # Tokenize tweet
+        for token in tokenized_tweet:
+            # Apply preprocessing to the token
+            token = token.lower() if self.lower else token
+            token = token.strip() if self.strip else token
+            yield token
+	
+
+def identity(arg):
+    """
+    Simple identity function works as a passthrough.
+    """
+    return arg
 	
 def readFile(file):
 	return pickle.load(open(file, 'rb'))
 	
 def classify(train_tweets, train_categories):
 	text_clf = Pipeline([('preprocessor', CustomPreprocessor()),
-						 ('vectorizer', TfidfVectorizer(tokenizer=None, preprocessor=None, lowercase=False)),
+						 ('vectorizer', TfidfVectorizer(tokenizer=identity, preprocessor=None, lowercase=False)),
 						 ('classifier', SGDClassifier(loss='hinge', penalty='l2',
 											   alpha=1e-3, random_state=42,
 											   max_iter=5, tol=None)),
