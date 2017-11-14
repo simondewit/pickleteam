6139f78eeee847b43ac8151a2d17f23993a85793
DESKTOP-KR66LM4\Remon <remonkruizinga@gmail.com>
2017-11-14 22:11:56 +0100

    LSTM

--- kerasNetwork.py
+++ kerasNetwork.py
@@ -1,27 +1,43 @@
-#acc: 0.3784
-#download glove.twitter.27B on https://nlp.stanford.edu/projects/glove/
-from __future__ import print_function
-
-import json
-import os
-import numpy as np
-from numpy import array
-
 import pickle
 import sys
+import os
+import numpy as np
+import string
 
 import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
+from nltk.tokenize import TweetTokenizer
+from sklearn import svm
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from nltk.corpus import stopwords as sw
+from nltk.corpus import wordnet as wn
+from nltk import wordpunct_tokenize
+from nltk import WordNetLemmatizer
+from nltk import sent_tokenize
+from nltk import pos_tag
+
+from createConfusionMatrix import main as mainCCM
 
 from keras.utils import to_categorical
 
 import keras
 import keras.backend as K
+
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing import sequence
+
+from keras.models import Model, Sequential
+
 from keras.layers import Dense, Input, Flatten
-from keras.layers import Conv1D, MaxPooling1D, Embedding
-from keras.models import Model
+from keras.layers import Embedding
+from keras.layers import LSTM
 
 BASE_DIR = ''
 GLOVE_DIR = BASE_DIR + 'glove.twitter.27B/'
@@ -31,6 +47,85 @@ EMBEDDING_DIM = 100
 VALIDATION_SPLIT = 0.2
 batch_size = 128
 epochs = 2
+
+def main():
+	#read documents
+	train_documents = readFile('eng-train.pickle')
+	test_documents = readFile('eng-trial.pickle')
+
+	#create seperate lists for tweets and the categories
+	train_tweets, train_categories = createLists(train_documents)
+	test_tweets, test_categories = createLists(test_documents)
+
+	texts = train_tweets + test_tweets
+	labels = train_categories + test_categories
+	tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
+	tokenizer.fit_on_texts(texts)
+	sequences = tokenizer.texts_to_sequences(texts)
+
+	word_index = tokenizer.word_index
+	print('Found %s unique tokens.' % len(word_index))
+
+	data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+	labels = to_categorical(np.asarray(labels))
+	print('Shape of data tensor:', data.shape)
+	print('Shape of label tensor:', labels.shape)
+
+	print(train_tweets[0])
+	train_tweets_trans = data[0:len(train_tweets)]
+	train_categories_trans = labels[0:len(train_categories)]
+
+	test_tweets_trans = data[len(train_tweets):]
+	test_categories_trans = labels[len(train_categories):]
+
+	embeddings_index = {}
+	f = open(os.path.join(GLOVE_DIR, 'glove.twitter.27B.100d.txt'), encoding="utf8")
+	for line in f:
+			values = line.split()
+			word = values[0]
+			coefs = np.asarray(values[1:], dtype='float32')
+			embeddings_index[word] = coefs
+	f.close()
+
+	embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+	for word, i in word_index.items():
+			embedding_vector = embeddings_index.get(word)
+			if embedding_vector is not None:
+					# words not found in embedding index will be all-zeros.
+					embedding_matrix[i] = embedding_vector
+
+	embedding_layer = Embedding(len(word_index) + 1,
+															EMBEDDING_DIM,
+															weights=[embedding_matrix],
+															input_length=MAX_SEQUENCE_LENGTH,
+															trainable=False)
+
+	print('Found %s word vectors.' % len(embeddings_index))
+
+	model = Sequential()
+	model.add(embedding_layer)
+	model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+	model.add(Dense(20, activation='sigmoid'))
+
+	model.compile(loss='categorical_crossentropy',
+								optimizer='rmsprop',
+								metrics=['accuracy', precision, recall]) 
+
+	print('Train...')
+	model.fit(train_tweets_trans, train_categories_trans,
+						batch_size=batch_size,
+						epochs=1,
+						validation_data=(test_tweets_trans, test_categories_trans))
+
+	score = model.evaluate(test_tweets_trans, test_categories_trans, batch_size=batch_size)
+
+	print("\n",score)
+
+	#print('Test score:', score[0])
+	#print('Test accuracy:', score[1])
+	print('Test precision:', score[2])
+	print('Test recall:', score[3])
+	print('\nTest fscore:', fscore(score[2],score[3]))
 
 def readFile(file):
 	return pickle.load(open(file, 'rb'))
@@ -75,8 +170,16 @@ def fscore(precision,recall):
 	fscore = 2 * ((precision * recall) / (precision + recall))
 	return fscore
 
+def createLists(documents):
+	tweets = []
+	categories = []
+	for category, tweet in documents:
+		categories.append(category)
+		tweets.append(tweet)
+	return tweets, categories
+	
 
-def main():
+def main1():
 	#read documents
 	documents = readFile('tweetsAsTuplesFile2.pickle')
 
@@ -124,7 +227,6 @@ def main():
 					# words not found in embedding index will be all-zeros.
 					embedding_matrix[i] = embedding_vector
 
-	from keras.layers import Embedding
 	embedding_layer = Embedding(len(word_index) + 1,
 															EMBEDDING_DIM,
 															weights=[embedding_matrix],
@@ -132,26 +234,20 @@ def main():
 															trainable=False)
 
 	print('Found %s word vectors.' % len(embeddings_index))
+	model = Sequential()
+	model.add(embedding_layer)
+	model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+	model.add(Dense(20, activation='sigmoid'))
 
-	sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-	embedded_sequences = embedding_layer(sequence_input)
-	x = Conv1D(128, 5, activation='relu')(embedded_sequences)
-	x = MaxPooling1D(5)(x)
-	x = Conv1D(128, 5, activation='relu')(x)
-	x = MaxPooling1D(5)(x)
-	x = Conv1D(128, 5, activation='relu')(x)
-	x = MaxPooling1D(35)(x)  # global max pooling
-	x = Flatten()(x)
-	x = Dense(128, activation='relu')(x)
-	preds = Dense(20, activation='softmax')(x)
-
-	model = Model(sequence_input, preds)
 	model.compile(loss='categorical_crossentropy',
 								optimizer='rmsprop',
-								metrics=['acc',precision,recall])
+								metrics=['accuracy', precision, recall]) 
 
+	print('Train...')
 	model.fit(x_train, y_train,
-						epochs=epochs, batch_size=batch_size)
+						batch_size=batch_size,
+						epochs=1,
+						validation_data=(x_val, y_val))
 
 	score = model.evaluate(x_val, y_val, batch_size=batch_size)
 
@@ -163,6 +259,8 @@ def main():
 	print('Test recall:', score[3])
 	print('\nTest fscore:', fscore(score[2],score[3]))
 
+	
+#acc: 0.3267 - precision: 0.5397 - recall: 0.1190 This method
 
 
 if __name__ == '__main__':
