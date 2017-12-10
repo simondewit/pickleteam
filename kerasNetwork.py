2d23b2a6253a4cc0b7da2db18ce9409f9bec1c63
Simon de Wit <simondewit@MacBook-Pro-van-Simon-de-Wit.local>
2017-12-10 15:57:43 +0100

    mergeeee...

--- kerasNetwork.py
+++ kerasNetwork.py
@@ -57,7 +57,7 @@ def main():
 	print('Found %s unique tokens.' % len(word_index))
 
 	data = pad_sequences(sequences)
-	import ipdb; ipdb.set_trace()
+	# import ipdb; ipdb.set_trace()
 	labels = to_categorical(list(map(int, labels)))
 	print('Shape of data tensor:', data.shape)
 	print('Shape of label tensor:', labels.shape)
