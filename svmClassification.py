58ea0b528b56eb3aec1677b73be7048de3436eb8
Simon de Wit <simondewit@RN-145-97-137-139.eduroam.rug.nl>
2017-12-12 14:23:11 +0100

    merge

--- svmClassification.py
+++ svmClassification.py
@@ -21,7 +21,8 @@ from nltk import WordNetLemmatizer
 from nltk import sent_tokenize
 from nltk import pos_tag
 
-from createConfusionMatrix import main as mainCCM
+#from createConfusionMatrix import main as mainCCM
+from nltk.stem import SnowballStemmer
 
 
 def main():
@@ -102,6 +103,13 @@ def identity(arg):
     """
     return arg
 
+def customStemmer(arg):
+	"""
+    Preprocesser function to test different stemmers.
+    """
+	stm = SnowballStemmer('english')
+	return stm.stem(arg) 
+
 def tweetIdentity(arg):
 	tokenizer = TweetTokenizer()
 	return tokenizer.tokenize(arg)
@@ -112,8 +120,8 @@ def readFile(file):
 def classify(train_tweets, train_categories):
 	#('preprocessor', CustomPreprocessor()),
 	text_clf = Pipeline([('feats', FeatureUnion([
-						 ('char', TfidfVectorizer(tokenizer=tweetIdentity, norm="l1", preprocessor=None, stop_words="english", lowercase=False, analyzer='char', ngram_range=(3,5), min_df=1)),#, max_features=100000)),
-						 ('word', TfidfVectorizer(tokenizer=tweetIdentity, norm="l1", preprocessor=None, stop_words="english", lowercase=False, analyzer='word', ngram_range=(1,3), min_df=1)),#, max_features=100000)),
+						 ('char', TfidfVectorizer(tokenizer=tweetIdentity, norm="l1", preprocessor=customStemmer, stop_words="english", lowercase=False, analyzer='char', ngram_range=(3,5), min_df=1)),#, max_features=100000)),
+						 ('word', TfidfVectorizer(tokenizer=tweetIdentity, norm="l1", preprocessor=customStemmer, stop_words="english", lowercase=False, analyzer='word', ngram_range=(1,3), min_df=1)),#, max_features=100000)),
 						 ])),
 						 ('classifier', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=50, tol=None))])
 	
